51ee5e70bf8bf159eab0fdcca8cc58636ffa4004
Xiao Guo <80196477+TuDouSeek@users.noreply.github.com>
2022-07-20 15:27:09 +0800

    Create HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,19 +16,55 @@
       <div id="container">
      <div class="search bar1">
             <form>
-                <input type="text" placeholder="请输入您要搜索的内容...">
+                <input type="text" style="border-radius:17px;" placeholder="请输入您要搜索的内容...">
                 <button type="submit" >  <iconfont-svg icon="icon-sousuo2" size="34" ><i ></i></iconfont-svg></button>
             </form>
         </div>
     </div>
     </div>
-    
+    <!-- 轮播图 -->
+    <div class="swiper">
+      <div class="swiper_list">
+
+      </div>
+      <div class="swiper_list">
+
+      </div>
+      <div class="swiper_list">
+
+      </div>
+      <div class="swiper_list">
+
+      </div>
+      <div class="swiper_list">
+
+      </div>
+    </div>
+    <!-- 菜单栏 -->
+    <div class="menu">
+      <div class="menu_list menu_listA" @click="menu(1)"><iconfont-svg class="menu_icon" icon="icon-daizhangguanli" size="34" ><i ></i></iconfont-svg></div>
+      <div class="menu_list menu_listB" @click="menu(2)"><iconfont-svg class="menu_icon" icon="icon-daizhangguanli" size="34" ><i ></i></iconfont-svg></div>
+      <div class="menu_list menu_listC" @click="menu(3)"><iconfont-svg class="menu_icon" icon="icon-daizhangguanli" size="34" ><i ></i></iconfont-svg></div>
+      <div class="menu_list menu_listD" @click="menu(4)"><iconfont-svg class="menu_icon" icon="icon-daizhangguanli" size="34" ><i ></i></iconfont-svg></div>
+      <div class="menu_list menu_listE" @click="menu(5)"><iconfont-svg class="menu_icon" icon="icon-daizhangguanli" size="34" ><i ></i></iconfont-svg></div>
+      <div class="menu_list menu_listF" @click="menu(6)"><iconfont-svg class="menu_icon" icon="icon-daizhangguanli" size="34" ><i ></i></iconfont-svg></div>
+      <div class="menu_list menu_listG" @click="menu(7)"><iconfont-svg class="menu_icon" icon="icon-daizhangguanli" size="34" ><i ></i></iconfont-svg></div>
+      <div class="menu_list menu_listH" @click="menu(8)"><iconfont-svg class="menu_icon" icon="icon-daizhangguanli" size="34" ><i ></i></iconfont-svg></div>
+      <div class="menu_list menu_listI" @click="menu(9)"><iconfont-svg class="menu_icon" icon="icon-daizhangguanli" size="34" ><i ></i></iconfont-svg></div>
+      <div class="menu_list menu_listJ" @click="menu(10)"><iconfont-svg class="menu_icon" icon="icon-daizhangguanli" size="34" ><i ></i></iconfont-svg></div>
+      <div class="menu_list menu_listK" @click="menu(11)"><iconfont-svg class="menu_icon" icon="icon-daizhangguanli" size="34" ><i ></i></iconfont-svg></div>
+      <div class="menu_list menu_listL" @click="menu(12)"><iconfont-svg class="menu_icon" icon="icon-daizhangguanli" size="34" ><i ></i></iconfont-svg></div>
+      <div class="menu_list menu_listM" @click="menu(13)"><iconfont-svg class="menu_icon" icon="icon-daizhangguanli" size="34" ><i ></i></iconfont-svg></div>
+      <div class="menu_list menu_listN" @click="menu(14)"><iconfont-svg class="menu_icon" icon="icon-daizhangguanli" size="34" ><i ></i></iconfont-svg></div>
+      <div class="menu_list menu_listO" @click="menu(15)"><iconfont-svg class="menu_icon" icon="icon-daizhangguanli" size="34" ><i ></i></iconfont-svg></div>
+    </div> 
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import axios from 'axios'
 export default {
   name: 'HomeView',
   data(){
@@ -38,10 +74,14 @@ export default {
     }
   },
   methods:{
-   
     kb(){
         this.isVisible = !this.isVisible;
         this.a =! this.a
+    },
+    menu(id){
+      axios.get('api/hello/type=' + id).then(res =>{
+        console.log(res);
+      })
     }
   }
 }
@@ -57,12 +97,13 @@ export default {
   height: 0;
   width: 0px;
   width: 100%;
-  transition: display 2s;
+
 }.sidebarA{
   width: 63%;
     height: 43vh;
     background: slategray;
     z-index: 2;
+    border-radius: 2px 2px 10px 2px;
 }.sidebarB{
     width: 100%;
     height: 100vh;
@@ -83,7 +124,7 @@ export default {
   margin-right: 18%;
 }
 .bar1 {
-    background: #A3D0C3;
+  
 }
 
 .bar1 input {
@@ -126,5 +167,76 @@ button {
     width: 35px;
     cursor: pointer;
     position: absolute;
+}
+/* 轮播图 */
+.swiper{
+  width: 96%;
+  height: 200px;
+  background: linear-gradient(47deg,#7117ea40,#dbd0df);
+  margin-top: 60px;
+  position: relative;
+   display: -webkit-inline-box;
+  overflow: auto;
+      transform: translate(2%);
+      border-radius: 10px;
+}.swiper_list{
+  float: left;
+  width: 120px;
+  height: 180px;
+  border-radius: 10px;
+  margin: 10px;
+  background: #F9F0DA;
+ 
+}
+/* 菜单栏 */
+.menu{
+  width: 96%;
+  margin-top: 10px;
+  border-radius: 10px;
+  display: flex;
+  transform: translate(2%);
+  flex-wrap: wrap;
+  justify-content: space-between;
+}.menu_list{
+  width: calc(19.4%);
+  height: 50px;
+  margin: 1px;
+  background: aquamarine;
+  position: relative;
+}.menu_listA{
+  border-radius: 5px 0px 0px 0px;background: linear-gradient(175deg,#fec163,#e28a6f);
+}.menu_listB{
+  background: linear-gradient(358deg,#17ead9,#60c3ea);
+}.menu_listC{
+   background: linear-gradient(1deg,#97abff,#619ffb);
+}.menu_listD{
+   background: linear-gradient(179deg,#2ad349,#57ca85);
+}.menu_listE{
+  border-radius: 0px 5px 0px 0px; background: linear-gradient(177deg,#eeff01,#dedfba);
+}.menu_listF{
+   background: #e5a74b75;
+}.menu_listG{
+   background: #57ccd670;
+}.menu_listH{
+   background: #916fcf70;
+}.menu_listI{
+   background: #6fcf7970;
+}.menu_listJ{
+   background: #c7cf6f70;
+}.menu_listK{
+  border-radius: 0px 0px 0px 5px; background: #e5a74b75;
+}.menu_listL{
+   background: #57ccd670;
+}.menu_listM{
+   background: #916fcf70;
+}.menu_listN{
+   background: #6fcf7970;
+}.menu_listO{
+  border-radius: 0px 0px 5px 0px; background: #c7cf6f70;
+}.menu_icon{
+  position: absolute;
+  transform: translate(50%,-50%);
+  top: 50%;
+  left: 0;
 }
 </style>
