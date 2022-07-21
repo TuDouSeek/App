e8870d709069583f00c4f3b87cc68532c9429df4
Xiao Guo <80196477+TuDouSeek@users.noreply.github.com>
2022-07-21 16:28:00 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,13 +3,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
 import Nav from '@/components/Nav.vue'
+import Harder from '@/components/harder.vue'
 import "./iconfont/iconfont.js";
 import iconfontSvg from "@/iconfont/iconfont.vue";
 import './iconfont/iconfont.css'
+import touch from 'vue-directive-touch';
 Vue.config.productionTip = false
 Vue.component("iconfont-svg",iconfontSvg)
 Vue.component('Nav',Nav)
+Vue.component('Harder',Harder)
+Vue.use(touch);
 new Vue({
   router,
   store,
