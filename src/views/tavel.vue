f8d323df97a99c882fc45473f54b417934225d36
Xiao Guo <80196477+TuDouSeek@users.noreply.github.com>
2022-07-21 16:27:24 +0800

    Create tavel.vue

---
+++ src/views/tavel.vue
@@ -0,0 +1,54 @@
+<template>
+  <div v-touch:right="callback">
+    <Harder></Harder>
+    <div class="a">
+      <div class="cd" v-for="index in res" :key="index.id">
+        <p>{{index.name}}</p>
+        <img :src="index.img" class="img" alt="" srcset="">
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data(){
+    return{
+      res:[]
+    }
+  },
+  created(){
+    this.a()
+  },
+  methods:{ 
+    a(){
+      axios.get('api/travel/' +this.$route.params.id).then(res =>{
+      
+       this.res = res.data
+      })
+    },
+    callback(){
+       this.$router.push({path : '/'})
+    }
+  }
+}
+</script>
+
+<style>
+.a{
+  display: flex;
+      flex-wrap: wrap;
+    justify-content: space-between;
+    margin: 0px 7%;
+}
+.cd{
+  width: 150px;
+  height: 200px;
+  background: rgba(194, 194, 187, 0.712);
+  float: left;
+  margin-top: 10px;
+}.img{
+  width: 100%;
+}
+</style>
