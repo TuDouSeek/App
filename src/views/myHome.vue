c400fc5e69b37bf73b07f2d02a1b2f244bacd795
Xiao Guo <80196477+TuDouSeek@users.noreply.github.com>
2022-07-29 15:53:05 +0800

    Create myHome.vue

---
+++ src/views/myHome.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="home">
+    <ul class="ul">
+      <li>
+         <p>111</p>
+         <img src="../assets/logo.png" alt="" srcset="">
+      </li>
+      <li>
+        222
+      </li>
+      <li>
+        333
+      </li>
+      <li>
+        444
+      </li>
+      <li>
+        555
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.home{
+  width: 100%;
+  height: 100%;
+}.ul>li{
+  width: 100%;
+  height: 60px;
+  position: relative;
+}.ul>li:hover{
+  background: rgb(189, 189, 189);
+}.ul>li>img{
+ width: 50px;
+ height: 50px;
+ line-height: 60px;
+ border-radius: 50%;
+ position: absolute;
+ right: 20;
+}
+</style>
